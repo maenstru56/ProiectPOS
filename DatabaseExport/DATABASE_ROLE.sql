f88a4a1add5c8f0d75d33023ba94170a74dca0f2
maenstru56 <peteleaza.darius@yahoo.com>
2021-12-17 16:04:44 +0200

    Database Export

--- DatabaseExport/DATABASE_ROLE.sql
+++
@@ -1,4 +0,0 @@
-insert into DATABASE.ROLE (ID, ROLE)
-values  (1, 'cashier'),
-        (2, 'director'),
-        (3, 'admin');
--- DatabaseExport/DATABASE_STATE.sql
+++
@@ -1,4 +0,0 @@
-insert into DATABASE.STATE (ID, STATE)
-values  (1, 'pending'),
-        (2, 'invalid'),
-        (3, 'valid');
